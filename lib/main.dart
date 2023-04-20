49ad3bc1f65a1b3c30f26875c6d5b6d3447b394c
Michel Feinstein <michel@feinstein.com.br>
2023-04-20 00:58:59 -0300

    Add home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_auth_exporter/data/repositories/qr_code_repository.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,55 +14,62 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
+        brightness: Brightness.dark,
+        canvasColor: const Color(0xFF202124),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          foregroundColor: Colors.white,
+          systemOverlayStyle: SystemUiOverlayStyle(
+              statusBarColor: Colors.transparent,
+            ),
+            elevation: 0,
+
+        )
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Google Auth Exporter'),
+        centerTitle: true,
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Google Authenticator generates a proprietary QR Code for exporting your 2FA to another device.\n\n'
+              "For security reasons, it's not possible to use the same device for scanning the QR code.\n\n"
+              'If you want to use this app from the same device that has Google Authenticator, take a picture '
+              'of the QR code with another device (computer, phone, etc.) '
+              'and then scan the picture with this app.\n\n'
+              "This app doesn't transmit any data to the internet and is open-sourced.",
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(height: 40.0),
+            ElevatedButton(
+              onPressed: () async {
+                final qrCode = await QrCodeRepository().scan();
+              },
+              child: const Text('Scan QR Code'),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
