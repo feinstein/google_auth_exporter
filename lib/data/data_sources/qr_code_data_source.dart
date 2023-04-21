0e6b008f39e0c9f23858f7490eb6d7ede2914926
Michel Feinstein <michel@feinstein.com.br>
2023-04-21 03:03:28 -0300

    Protect the QR Code reading for Android only

--- lib/data/data_sources/qr_code_data_source.dart
+++ lib/data/data_sources/qr_code_data_source.dart
@@ -1,7 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 class QrCodeDataSource {
   static const channel = MethodChannel('barcode_scanner');
 
-  Future<String?> scan() async => channel.invokeMethod<String>('scan');
+  Future<String?> scan() async {
+    if (defaultTargetPlatform != TargetPlatform.android) {
+      return null;
+    }
+
+    return channel.invokeMethod<String>('scan');
+  }
 }
